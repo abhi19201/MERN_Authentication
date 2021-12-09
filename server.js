d40d871c854c512a5f82ebe4cecd76f5a00860db
Abhijeet Wankhade <abhijeetwankhade2001@gmail.com>
2021-12-09 18:48:22 +0530

    cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
